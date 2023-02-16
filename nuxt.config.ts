918fed839d617f20e54602d3f0443adb93343884
Roduan Kareem Aldeen <37983260+RoduanKD@users.noreply.github.com>
2023-02-16 14:05:05 +0300

    optimize bundle size
    Took 15 minutes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import vuetify from 'vite-plugin-vuetify'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
@@ -22,4 +24,11 @@ export default defineNuxtConfig({
   imports: {
     dirs: ['./utils'],
   },
+  modules: [
+    async (options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', config => config.plugins.push(
+        vuetify(),
+      ))
+    },
+  ],
 })
